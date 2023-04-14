2e0e7195ac627fe02f25a7742c9ff306b3956fc5
farhansolodev <farhanalvi005@gmail.com>
2023-04-14 21:44:23 +0400

    add perf stats window

--- game.go
+++ game.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/hajimehoshi/ebiten/v2/vector"
+	"golang.org/x/image/font/basicfont"
 )
 
 type Game[T Locatable] struct {
@@ -47,17 +50,29 @@ func (g *Game[T]) Draw(screen *ebiten.Image) {
 
 	width, height := ebiten.WindowSize()
 	x, y := ebiten.CursorPosition()
-	if x <= 0 || x > width || y <= 0 || y > height {
-		return
+	if x > 0 && x <= width && y > 0 && y <= height {
+		g.root.forEach(func (node *QNode[T]) bool {
+			if !node.marked {
+				return true
+			}
+			midX, midY := node.getMidValues()
+			vector.StrokeLine(screen, node.x0, midY, node.x1, midY, strokeThickness, color.White, false)
+			vector.StrokeLine(screen, midX, node.y0, midX, node.y1, strokeThickness, color.White, false)
+			return false
+		}, g.maxDepth)
 	}
 
-	g.root.forEach(func (node *QNode[T]) bool {
-		if !node.marked {
-			return true
-		}
-		midX, midY := node.getMidValues()
-		vector.StrokeLine(screen, node.x0, midY, node.x1, midY, strokeThickness, color.White, false)
-		vector.StrokeLine(screen, midX, node.y0, midX, node.y1, strokeThickness, color.White, false)
-		return false
-	}, g.maxDepth)
+	drawDebug(screen, 
+		fmt.Sprintf("TPS: %.2f", ebiten.CurrentTPS()), 
+		fmt.Sprintf("FPS: %.2f", ebiten.CurrentFPS()),
+		fmt.Sprintf("Sprites: %d", len(g.root.datapoints)),
+	)
+}
+
+func drawDebug(screen *ebiten.Image, msgs ...string) {
+	vector.DrawFilledRect(screen, 0, 0, initialScreenWidth*.1, initialScreenHeight*.035 * float32(len(msgs)), color.RGBA{0x7f, 0x00, 0x7f, 0x7f}, true)
+	font := text.FaceWithLineHeight(basicfont.Face7x13, 0)
+	for i, msg := range msgs {
+		text.Draw(screen, msg, font, 5, (i+1)*20, color.White)
+	}
 }
